e3e5d2fb35f6d4281aa51741f541e16e7fedde54
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-17 20:48:10 +0800

    refactor: 提取出不变的样式以加快编译速度

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './index.less';
+import './index.css';
 import './webkit-scrollbar.css';
 
 ReactDOM.render(
