e61afc697d4c8dee6206881de313b66b7a00fc1c
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-18 23:16:40 +0800

    feat: 画出对称的另一半频率图

--- src/utils/draw-frequency.ts
+++ src/utils/draw-frequency.ts
@@ -19,15 +19,36 @@ export function drawFrequencyHistogram(
   ctx.fillStyle = fillStyle;
   ctx.fillRect(0, 0, width, height);
 
-  const barWidth = (width / bufferLength) * 2.5;
-  let x = 0;
+  const barWidth = width / bufferLength;
+  const heightRate = 0.5;
 
-  for (let i = 0; i < bufferLength; i++) {
+  let x = width / 2;
+  for (let i = 0; i < bufferLength && x < width; i++) {
     const barHeight = dataBuffer[i];
 
     ctx.fillStyle = `rgb(50, ${barHeight + 100}, 50)`;
-    ctx.fillRect(x, height - barHeight / 2, barWidth, barHeight / 2);
+    ctx.fillRect(
+      x,
+      height - barHeight * heightRate,
+      barWidth,
+      barHeight * heightRate
+    );
 
     x += barWidth + 1;
+  }
+
+  x = width / 2 - barWidth - 1;
+  for (let i = 0; i < bufferLength && x > -barWidth; i++) {
+    const barHeight = dataBuffer[i];
+
+    ctx.fillStyle = `rgb(50, ${barHeight + 100}, 50)`;
+    ctx.fillRect(
+      x,
+      height - barHeight * heightRate,
+      barWidth,
+      barHeight * heightRate
+    );
+
+    x -= barWidth + 1;
   }
 }
