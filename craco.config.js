fae0354a1622cb5bf246c28c563c07d8e9d83085
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-11-09 13:44:30 +0800

    feat: 配置暗黑主题

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,21 @@
 const CracoAntDesignPlugin = require('craco-antd');
+const { getThemeVariables } = require('antd/dist/theme');
 
 module.exports = {
   plugins: [
     {
       plugin: CracoAntDesignPlugin,
       options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              ...getThemeVariables({
+                dark: true,
+              }),
+              'border-color-base': '#847f7f',
+            },
+          },
+        },
         babelPluginImportOptions: {
           libraryDirectory: 'es',
         },
